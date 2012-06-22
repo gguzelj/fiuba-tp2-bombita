5d0009b30e5ee306172f4eebaa47608898302b6c
German.Guzelj@gmail.com <German.Guzelj@gmail.com@7e8660cf-6aed-79bb-f480-d60cee213790>
2012-06-22 03:52:48 +0000

    Version correcta

---
+++ Bomberman/src/com/bombitarodriguez/vista/factory/VistaBloqueCemento.java
@@ -0,0 +1,12 @@
+package com.bombitarodriguez.vista.factory;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class VistaBloqueCemento extends Imagen{
+	
+	public VistaBloqueCemento(){
+
+		this.setNombreArchivoImagen("/com/bombitarodriguez/vista/Bloque_Cemento.png");
+		
+	}
+}
