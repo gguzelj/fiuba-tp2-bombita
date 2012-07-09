56c7717e4ec766b0f671c959a9d315f75abb9c00
German.Guzelj@gmail.com <German.Guzelj@gmail.com@7e8660cf-6aed-79bb-f480-d60cee213790>
2012-07-09 22:33:20 +0000

    -Agrego FactoryVistas, VistaArticuloChala, VistaArticuloTimer e Identificaciones

---
+++ trunk/Bomberman/src/com/bombitarodriguez/vista/factory/dominio/FactoryVistas.java
@@ -0,0 +1,63 @@
+package com.bombitarodriguez.vista.factory.dominio;
+
+import com.bombitarodriguez.excepciones.IdInexistente;
+import com.bombitarodriguez.utils.Identificaciones;
+
+import ar.uba.fi.algo3.titiritero.vista.Imagen;
+
+public class FactoryVistas {
+
+	public static Imagen getVistaPorId(int ID) throws IdInexistente{
+		
+		switch (ID) {
+		case Identificaciones.bombita:
+			return new VistaBombita();
+		
+		case Identificaciones.cecilio:
+			return new VistaCecilio();
+
+		case Identificaciones.lopezReggae:
+			return new VistaLopezReggae();
+			
+		case Identificaciones.lopezReggaeAlado:
+			return new VistaLopezReggaeAlado();
+			
+		case Identificaciones.articuloChala:
+			return new VistaArticuloChala();
+		
+		case Identificaciones.articuloTimer:
+			return new VistaArticuloTimer();
+			
+		case Identificaciones.articuloToleTole:
+			return new VistaArticuloToleTole();
+			
+		case Identificaciones.bloqueLadrillo:
+			return new VistaBloqueLadrillo();
+			
+		case Identificaciones.bloqueCemento:
+			return new VistaBloqueCemento();
+
+		case Identificaciones.bloqueAcero:
+			return new VistaBloqueAcero();
+
+		case Identificaciones.molotov:
+			return new VistaMolotov();
+
+		case Identificaciones.proyectil:
+			return new VistaProyectil();
+
+		case Identificaciones.toleTole:
+			return new VistaToleTole();
+
+		case Identificaciones.salida:
+			return new VistaSalida();
+			
+		case Identificaciones.explosion:
+			return new VistaExplosion();
+			
+		default:
+			System.out.println(ID);
+			throw new IdInexistente("ID Inexistente");
+		}
+	}
+}
