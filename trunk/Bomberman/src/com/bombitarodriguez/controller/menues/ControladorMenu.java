392347e7d1ca4dde58022fd509ce9dabdbcf90fb
German.Guzelj@gmail.com <German.Guzelj@gmail.com@7e8660cf-6aed-79bb-f480-d60cee213790>
2012-07-14 16:48:14 +0000

    -ControladorMenu refactorizado -Se eliminaron clases sin utilizar

--- trunk/Bomberman/src/com/bombitarodriguez/controller/menues/ControladorMenu.java
+++ trunk/Bomberman/src/com/bombitarodriguez/controller/menues/ControladorMenu.java
@@ -14,7 +14,7 @@ public class ControladorMenu implements KeyPressedObservador {
 	public ControladorMenu(Menu menu) {
 		this.menu = menu;
 		this.itemActual = 0;
-		this.cantidadDeItems = this.menu.getItems().size();
+		this.cantidadDeItems = this.menu.getCantidadDeItems();
 	}
 
 	@Override
@@ -23,26 +23,50 @@ public class ControladorMenu implements KeyPressedObservador {
 		switch (event.getKeyCode()) {
 
 		case KeyEvent.VK_DOWN:
-			this.menu.getItems().get(itemActual).getVistaItem().itemOut();
+			this.itemOut();
 			this.itemActual++;
 			this.validarIndice();
-			this.menu.getItems().get(itemActual).getVistaItem().itemOver();
+			this.itemOver();
 			break;
 
 		case KeyEvent.VK_UP:
-			this.menu.getItems().get(itemActual).getVistaItem().itemOut();
+			this.itemOut();
 			this.itemActual--;
 			this.validarIndice();
-			this.menu.getItems().get(itemActual).getVistaItem().itemOver();
+			this.itemOver();
 			break;
 
 		case KeyEvent.VK_ENTER:
-			this.menu.getItems().get(itemActual).seleccionar();
+			this.itemSeleccionar();
 			break;
 		}
 	}
+
+	/**
+	 * Seleccionamos el item actual
+	 */
+	private void itemSeleccionar() {
+		this.menu.itemSeleccionar(itemActual);
+	}
+
+	/**
+	 * El cursor pasa sobre el item actual
+	 */
+	private void itemOver() {
+		this.menu.itemOver(itemActual);
+	}
+
+	/**
+	 * El cursor se va del item actual
+	 */
+	private void itemOut() {
+		this.menu.itemOut(itemActual);
+	}
 	
-	/*Validacion para verificar que el indice se encuentre dentro de los items*/
+	/**
+	 * Validacion para verificar que el indice 
+	 * se encuentre dentro de los items
+	 */
 	protected void validarIndice() {
 		if (this.itemActual < 0)
 			this.itemActual = this.cantidadDeItems - 1;
